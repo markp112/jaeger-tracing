0c4346cb7a84c8bedb03b527e66fa285e5b9cafb
mark phillips <mark.phillips1965@googlemail.com>
2023-08-17 18:13:16 +0200

    added logging to auth app=

--- auth-app/src/api/auth/auth.ts
+++ auth-app/src/api/auth/auth.ts
@@ -16,12 +16,12 @@ authRouter.post(getPath('login'), async (req: Request, res: Response) => {
     logger.error('invalid credentials - Missing');
     res.status(400).send('missing credentials')
   }
-  if (bodyData.userName) {
+  // if (bodyData.userName) {
     const credentials: Credential = bodyData;
     const authService = new AuthService(new AuthRepository());
     const loggedIn = await authService.login(credentials);
     res.status(200).send(loggedIn);
-  }
+  // }
 });
 
 export { authRouter };
