8adc7a0b9cf28ea68c2c3550ef8aad3b625764a7
mark phillips <mark.phillips1965@googlemail.com>
2023-08-18 11:03:37 +0200

    adding postgre

---
+++ auth-app/prisma/migrations/20220826203628_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "description" TEXT,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
