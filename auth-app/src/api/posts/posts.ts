c8f4ead70cdfee5e63f29064017af4f6ca564f84
Mark-Arnaud Phillips <mark-arnaud.phillips@altran.com>
2023-08-21 16:07:25 +0200

    added code to support posts

--- auth-app/src/api/posts/posts.ts
+++ auth-app/src/api/posts/posts.ts
@@ -18,6 +18,7 @@ postsRouter.post(getPath(''), async (req: Request, res: Response) => {
       const prisma = new PrismaClient();
       const postService = new PostService(new PostRepository(prisma));
       const posts = await postService.fetchPosts();
+      logger.info(`posts retrieved -->${posts}`);
       requestSpan.setAttribute('http.status', HTTP_STATUS.OK);
       res.status(HTTP_STATUS.OK).send(posts);
     } catch (err) {
