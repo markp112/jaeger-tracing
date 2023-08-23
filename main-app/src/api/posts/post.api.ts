80b6a2ed0f895a681adc8a5ca522149d3412535e
Mark-Arnaud Phillips <mark-arnaud.phillips@altran.com>
2023-08-23 17:51:16 +0200

    experiment with decorators

--- main-app/src/api/posts/post.api.ts
+++ main-app/src/api/posts/post.api.ts
@@ -3,7 +3,7 @@ import { PostsService } from '@core/service/posts/posts.service';
 import { PostType } from '@model/posts/posts.model';
 import { PostsRepository } from '@repository/posts/posts.repository';
 import { Config } from '../../config/config';
-import { Request } from 'express';
+import { logger } from '@logger/logger';
 
 type Result = {
   count: number;
@@ -13,8 +13,8 @@ type Result = {
 
 class PostApi {
   @traceRequest('get posts')
-  async getPosts(req: Request): Promise<Result> {
-    req.log.info('posts Api called');
+  async getPosts(): Promise<Result> {
+    logger.info('posts Api called');
     const baseUrl = new Config.AuthUrl().getUrl();
     const postsService = new PostsService(new PostsRepository(baseUrl));
     const postResult: PostType[] = await postsService.fetchPosts();
