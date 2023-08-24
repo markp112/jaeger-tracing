9a4510fd29543282dafc4ff6dcc8cdb9388785d9
Mark-Arnaud Phillips <mark-arnaud.phillips@altran.com>
2023-08-24 09:53:22 +0200

    refinign errors

--- main-app/src/api/decorators/tracing/tracing.decorator.ts
+++ main-app/src/api/decorators/tracing/tracing.decorator.ts
@@ -11,15 +11,6 @@ function traceRequest(endPoint: string) {
       return await tracer.startActiveSpan(endPoint, async (requestSpan) => {
         try {
           const returnValue = await method.apply(this, arguments);
-          // if (returnValue instanceof Promise) {
-          //   (async () => {
-          //     try {
-          //       await returnValue;
-          //     } catch (e) {
-          //       throw new Error(e);
-          //     }
-          //   })();
-          // }
           return returnValue;
         } catch (e) {
           requestSpan.setAttribute('http.status', 500);
@@ -31,38 +22,5 @@ function traceRequest(endPoint: string) {
     };
   };
 }
-// Side effect code here
-//
-// if ( returnValue instanceof Promise ) {
-//   ( async () => {
-//     try {
-//       await returnValue;
-//     } catch (e) {
-//       console.error('e', e);
-
-//       // alert(opts.i18nMessage);
-//     }
-//   } )();
-// }
-
-// return returnValue;
-// }
-
-// function traceRequest(target: Function, context) {
-//   if (context.kind === 'method') {
-//     return async function (...args: any[]) {
-//       await tracer.startActiveSpan('Get posts/', async (requestSpan) => {
-//         try {
-//           return await target.apply(this, args);
-//         } catch (e) {
-//           requestSpan.setAttribute('http.status', 500);
-//           requestSpan.recordException(`${(e as Error).message}`);
-//         } finally {
-//           requestSpan.end();
-//         }
-//       });
-//     };
-//   }
-// }
 
 export { traceRequest };
--- main-app/src/api/posts/posts.ts
+++ main-app/src/api/posts/posts.ts
@@ -14,38 +14,14 @@ const getPath = (pathToAppend: string) => `${ROUTE_PATH}/${pathToAppend}`;
 
 postsRouter.get(getPath(''), async (req: Request, res: Response) => {
   logger.child({ name: 'Posts' });
-  const postApi = new PostApi();
-  const postResult = await postApi.getPosts();
-  logger.info(`post result ${JSON.stringify(postResult)}`);
-  res.status(200).send(postResult);
-
-  // await tracer.startActiveSpan('Get posts/', async (requestSpan) => {
-  //   try {
-  //     const baseUrl = new Config.AuthUrl().getUrl();
-  //     const postsService = new PostsService(new PostsRepository(baseUrl));
-  //     const postResult: PostType[] = await postsService.fetchPosts();
-  //     if (postResult) {
-  //       const result = {
-  //         count: postResult.length,
-  //         firstRecord: postResult[0],
-  //         lastRecord: postResult[postResult.length - 1],
-  //       };
-  //       requestSpan.setAttribute('http.status', 200);
-  //       res.status(200).send(result);
-  //     } else {
-  //       requestSpan.recordException(JSON.stringify('result is undefined!'));
-  //       requestSpan.setAttribute('http.status', 200);
-  //       res.status(200).send('result is undefined');
-  //     }
-  //   } catch (e) {
-  //     logger.error(`error caught ->> ${(e as Error).message}`);
-  //     requestSpan.setAttribute('http.status', 500);
-  //     requestSpan.recordException(`${(e as Error).message}`);
-  //     res.status(500).json({ error: 500, details: `${(e as Error).message}` });
-  //   } finally {
-  //     requestSpan.end();
-  //   }
-  // });
+  try {
+    const postApi = new PostApi();
+    const postResult = await postApi.getPosts();
+    logger.info(`post result ${JSON.stringify(postResult)}`);
+    res.status(200).send(postResult);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 export { postsRouter };
